91058401389dac9201e804339af08619ff6fa2c9
Luis Gustavo <luisbarbosa.dev@gmail.com>
2024-11-03 13:18:41 -0300

    first configuration

---
+++ src/pages/app/statistics.tsx
@@ -0,0 +1,10 @@
+import { Helmet } from 'react-helmet-async'
+
+export function Statistics() {
+  return (
+    <>
+      <Helmet title="Estatística" />
+      <div>stats</div>
+    </>
+  )
+}
---
+++ src/routes.tsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+import { AppLayout } from './pages/_layouts/app'
+import { LandingPage } from './pages/app/landing-page'
+import { Statistics } from './pages/app/statistics'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [
+      { path: '/', element: <LandingPage /> },
+      { path: '/estatisticas', element: <Statistics /> },
+    ],
+  },
+])
